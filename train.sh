0edf84bd2ea526517b4365431e8f019dc9c3111b
Justin Guese <guese.justin@gmail.com>
2025-08-01 10:52:47 +0200

    add debug settings

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting ISBG training job..."
-
+echo "Mailbox: ${IMAP_USER}, Name: ${IMAP_HOST}, Good box: ${LEARN_HAMBOX}, Bad box: ${LEARN_SPAMBOX}"
 isbg \
     --imaphost="${IMAP_HOST}" \
     --imapuser="${IMAP_USER}" \
